e3d97a05b68f18c91bf65c3894d1847a2a678371
frelikh <renata@physics.ucsb.edu>
2017-05-17 08:44:20 -0700

    added mask by mass

--- plotting.py
+++ plotting.py
@@ -1,9 +1,12 @@
-infile = "/home/renata/rebound/examples/foo.txt"
+infile = "/home/renata/rebound/examples/foo2.txt"
 import numpy as np
-a,e = np.loadtxt(infile, usecols=(1,2),unpack=True,skiprows=1)
+a,e,m = np.loadtxt(infile, usecols=(1,2,7),unpack=True,skiprows=1)
 from matplotlib import pyplot as plt
-plt.scatter(a,e,s=5)
+mask = m>0.001
+plt.scatter(a,e,s=5,label="m<MJ",color='r')
+plt.scatter(a[mask],e[mask],color='b',s=5,label="m>MJ")
 plt.xscale('log')
+plt.legend()
 
 #semimajor_axis = np.linspace(0.01,2.,20)
 semimajor_axis = np.logspace(-2.0,1.0,100)
@@ -21,8 +24,8 @@ def v_kep(a):
 print(v_esc)
 
 es = [v_esc/v_kep(item) for item in semimajor_axis]
-plt.plot(semimajor_axis,es,color='r')
+plt.plot(semimajor_axis,es,color='g')
 plt.xlabel("Semimajor axis, AU")
 plt.ylabel("Eccentricity")
-plt.title("IC: Random a (0-5 AU) & true anomalies (0-2pi), masses 0.1-2 MJ, 100 runs, n=10")
+plt.title("IC: Random a=0-5AU & true anomaly=0-2pi, m=0.1-2 MJ, 100 runs, n=10")
 plt.savefig("foo.png")
